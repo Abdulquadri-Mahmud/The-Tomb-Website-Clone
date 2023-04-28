19ee851f168fa4763a6f43f1439602cdc5bf9327
Abdul-Quadri Mahmud Adeyemi <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2023-04-28 12:27:58 +0100

    Add files via upload

---
+++ The Tomb/smtp js/script.js
@@ -0,0 +1,13 @@
+const name = document.querySelector('#name');
+const lastname = document.querySelector('#lastname');
+const email = document.querySelector('#email');
+const number = document.querySelector('#number');
+
+const submit = document.querySelector('.form')[0];
+
+
+
+//FF3E486712DCD7186675F86025B1E43612B2
+
+// security token
+//d6fc18b1-13ea-4300-a035-8240b2143493
